80a93865c8b9c8fb7652b6e6dec87f0a8f87e139
plaze <banikakash131@gamil.com>
2023-04-07 10:19:38 +0530

    snake and food added

--- main.js
+++ main.js
@@ -6,17 +6,38 @@ const rows = 20;
 const column = 20;
 let context;
 
+//snake
+let snakeX = boxSize*5;
+let snakeY = boxSize*5;
+
+///food
+let foodX;
+let foodY;
+
 window.onload = function(){
     board.BoardHeight = boxSize*rows;
     board.BoardWidth = boxSize*column;
-
+//context doesnot take object it takes elem
     context=board.boardElem.getContext("2d");
-
+    placeFood();
     update();
 }
 
 function update(){
     context.fillStyle = "black";
+    console.log(board.BoardHeight);
     context.fillRect(0, 0, board.BoardWidth,board.BoardHeight);
+
+    context.fillStyle="lime";
+    context.fillRect(snakeX,snakeY,boxSize,boxSize);
+
+    context.fillStyle="red";
+    context.fillRect(foodX,foodY,boxSize,boxSize);
+
+}
+
+function placeFood(){
+    foodX = Math.floor(Math.random()*column)*boxSize;
+    foodY = Math.floor(Math.random()*rows)*boxSize;
 }
 
